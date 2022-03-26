7d41b07a392bd7ef702259815a9e767f4d3dc7c4
Gabriel Oliveira <gboliveira2503@gmail.com>
2022-03-25 21:55:21 -0300

    pratica

--- src/components/Teste.js
+++ src/components/Teste.js
@@ -1,31 +1,40 @@
-
-
-import{useState} from'react'
-
+import {useState} from 'react'
 function Teste(){
-    function cadastrarUsuario(e){
+    function meuCadastro(e){
         e.preventDefault()
-        console.log(`Usuário ${name} foi cadastrado com a senha: ${password}`)
+        console.log(`o ussuário: ${name} logou com a senha ${password}`)
     }
 
-    const [name,setName] = useState()
-    const [password,setPassword] = useState()
+    const [name,setName]= useState()
+    const [password,setPassword]=useState()
+
+
     return(
-        <div>
-            <h1>Meu  cadastro:</h1>
-            <form onSubmit={cadastrarUsuario}>
+        <div> 
+            <form onSubmit={meuCadastro}>
+                
+                <h1>Meu cadastro</h1>
                 <div>
-                    <label htmlFor="name">Nome:</label>
-                    <input type="text" id="name" name="name" placeholder="Digite o seu nome" onChange={(e) => setName(e.target.value)}/>
-                </div>
+                    <label htmlFor="name">Nome: </label>
+                    <input type="text" id="name" name="name" placeholder="digite seu nome" onChange={(e)=>setName(e.target.value)}/>
+                   </div> 
                 <div>
-                    <label htmlFor="password">Senha: </label>
-                    <input type="passsword" id="password" name="password" placeholder="Digite sua senha" onChange={(e) => setPassword(e.target.value)}/>
-                </div>
-                <div>
-            <input type="submit" value="cadastrar"></input>
-                </div>
-            </form>
+                        <label htmlFor="password">Senha: </label>
+                        <input type="password" id="password" name="password" placeholder="digite sua senha" onChange={(e)=>setPassword(e.target.value)}/>
+                         
+                         
+                            
+                    </div>
+                    <div>
+                        <input type="submit" value="cadastrar"/>
+                        </div>
+                    
+                
+                
+                </form>
+
+        
+        
         </div>
     )
 }
